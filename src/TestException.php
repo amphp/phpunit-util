39e9acd03f8a1c4e04e925a7637c68e161d3231e
Aaron Piotrowski <aaron@trowski.com>
2017-04-27 23:33:44 -0500

    Add TestException

---
+++ src/TestException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Amp\PHPUnit;
+
+/**
+ * Generic exception to be used in cases where an exception should be thrown and caught, but if the exception is not
+ * thrown, the test should fail using $this->fail(...).
+ *
+ * try {
+ *     functionCallThatShouldThrow();
+ *     $this->fail("Expected functionCallThatShouldThrow() to throw.");
+ * } catch (TestException $e) {
+ *     $this->assertSame($expectedExceptionInstance, $e);
+ * }
+ */
+class TestException extends \Exception {
+}
