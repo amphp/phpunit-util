1b77e877a279df533db3de5a5f2d4fa2be0c45e3
Charles Sprayberry <cspray@gmail.com>
2018-06-25 17:30:07 -0400

    adjustments based on 1st round of feedback

--- src/AsyncTestCase.php
+++ src/AsyncTestCase.php
@@ -1,30 +1,30 @@
-<?php declare(strict_types=1);
-
-/**
- * A PHPUnit TestCase intended to help facilitate writing async tests by running each test on the amphp Loop and
- * ensuring that the test runs until completion based on your test returning either a Promise or a Generator.
- */
+<?php
 
 namespace Amp\PHPUnit;
 
 use Amp\Loop;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 use function Amp\call;
+use function Amp\Promise\wait;
 
+/**
+ * A PHPUnit TestCase intended to help facilitate writing async tests by running each test on the amphp Loop and
+ * ensuring that the test runs until completion based on your test returning either a Promise or a Generator.
+ */
 abstract class AsyncTestCase extends PHPUnitTestCase {
 
     public function runTest() {
-        Loop::run(function() {
-            $testTimeout = $this->getTestTimeout();
-            $watcherId = Loop::delay($testTimeout, function() use($testTimeout) {
-                Loop::stop();
-                $this->fail('Expected test to complete before ' . $testTimeout . 'ms time limit');
-            });
-            yield call(function() {
-                return parent::runTest();
-            });
-            Loop::cancel($watcherId);
+        $testTimeout = $this->getTestTimeout();
+        $watcherId = Loop::delay($testTimeout, function() use($testTimeout) {
+            Loop::stop();
+            $this->fail('Expected test to complete before ' . $testTimeout . 'ms time limit');
         });
+
+        $returnValue = wait(call(function() {
+            return parent::runTest();
+        }));
+        Loop::cancel($watcherId);
+        return $returnValue;
     }
 
     protected function getTestTimeout() : int {
