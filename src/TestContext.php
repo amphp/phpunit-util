063da7198868b05f7c78a8bcd0a7a1f40c022027
Charles Sprayberry <cspray@gmail.com>
2018-06-25 17:31:47 -0400

    move docblock, remove strict_types in TestContext

--- src/TestContext.php
+++ src/TestContext.php
@@ -1,10 +1,4 @@
-<?php declare(strict_types=1);
-
-/**
- * A test helper meant to facilitate the returning of Promises to tests in an AsyncTestCase and to
- * ensure that a given number of async tasks are run by requiring a number of checkpoint() method
- * calls to be invoked during the test run.
- */
+<?php
 
 namespace Amp\PHPUnit;
 
@@ -12,6 +6,11 @@ use Amp\Deferred;
 use Amp\Promise;
 use Throwable;
 
+/**
+ * A test helper meant to facilitate the returning of Promises to tests in an AsyncTestCase and to
+ * ensure that a given number of async tasks are run by requiring a number of checkpoint() method
+ * calls to be invoked during the test run.
+ */
 final class TestContext {
 
     private $deferred;
