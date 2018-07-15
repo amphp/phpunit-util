8acccafc21423bcaddd50a346ca82de538c3232c
Niklas Keller <me@kelunik.com>
2018-07-15 18:35:12 +0200

    Disable loop replacement
    The LoopReset will check that no watchers are leaked later.

--- src/LoopReset.php
+++ src/LoopReset.php
@@ -2,13 +2,13 @@
 
 namespace Amp\PHPUnit;
 
-use Amp\Loop;
 use PHPUnit\Framework\BaseTestListener;
 use PHPUnit\Framework\Test;
 
-class LoopReset extends BaseTestListener {
-    public function endTest(Test $test, $time) {
-        Loop::set((new Loop\DriverFactory)->create());
+class LoopReset extends BaseTestListener
+{
+    public function endTest(Test $test, $time)
+    {
         gc_collect_cycles(); // extensions using an event loop may otherwise leak the file descriptors to the loop
     }
 }
