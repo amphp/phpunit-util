b1b727f393038dde0d27b95671e294ad2b230e6c
Niklas Keller <me@kelunik.com>
2018-07-15 22:22:17 +0200

    Warn if event loop has remaining watchers after test

--- src/LoopReset.php
+++ src/LoopReset.php
@@ -2,6 +2,7 @@
 
 namespace Amp\PHPUnit;
 
+use Amp\Loop;
 use PHPUnit\Framework\BaseTestListener;
 use PHPUnit\Framework\Test;
 
@@ -10,5 +11,18 @@ class LoopReset extends BaseTestListener
     public function endTest(Test $test, $time)
     {
         gc_collect_cycles(); // extensions using an event loop may otherwise leak the file descriptors to the loop
+
+        $info = Loop::getInfo();
+        $total = 0;
+
+        foreach (["defer", "delay", "repeat", "on_readable", "on_writable", "on_signal"] as $type) {
+            foreach (["enabled", "disabled"] as $state) {
+                $total += $info[$type][$state];
+            }
+        }
+
+        if ($total !== 0) {
+            \trigger_error("Event loop has remaining watchers after test: " . \json_encode($info, \JSON_PRETTY_PRINT), \E_USER_WARNING);
+        }
     }
 }
