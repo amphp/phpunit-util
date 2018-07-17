4c5fa5cfdb2172ac8353c74f802d308f67cb6aac
Niklas Keller <me@kelunik.com>
2018-07-17 19:42:50 +0200

    Report only leaked watchers from current test

--- src/LoopReset.php
+++ src/LoopReset.php
@@ -8,17 +8,31 @@ use PHPUnit\Framework\Test;
 
 class LoopReset extends BaseTestListener
 {
+    private $watcherCount;
+    private $previousInfo;
+
     public function startTest(Test $test)
     {
         Loop::setErrorHandler(function (\Throwable $error) {
             \trigger_error((string) $error, \E_USER_ERROR);
         });
+
+        $this->watcherCount = $this->countWatchers();
+        $this->previousInfo = Loop::getInfo();
     }
 
     public function endTest(Test $test, $time)
     {
         gc_collect_cycles(); // extensions using an event loop may otherwise leak the file descriptors to the loop
 
+        if ($this->countWatchers() - $this->watcherCount !== 0) {
+            $infoDiff = $this->calculateInfoDiff($this->previousInfo);
+            \trigger_error("Event loop has remaining watchers after test: " . \json_encode($infoDiff, \JSON_PRETTY_PRINT), \E_USER_WARNING);
+        }
+    }
+
+    private function countWatchers()
+    {
         $info = Loop::getInfo();
         $total = 0;
 
@@ -28,8 +42,19 @@ class LoopReset extends BaseTestListener
             }
         }
 
-        if ($total !== 0) {
-            \trigger_error("Event loop has remaining watchers after test: " . \json_encode($info, \JSON_PRETTY_PRINT), \E_USER_WARNING);
+        return $total;
+    }
+
+    private function calculateInfoDiff(array $previous): array
+    {
+        $info = Loop::getInfo();
+
+        foreach (["defer", "delay", "repeat", "on_readable", "on_writable", "on_signal"] as $type) {
+            foreach (["enabled", "disabled"] as $state) {
+                $info[$type][$state] -= $previous[$type][$state];
+            }
         }
+
+        return $info;
     }
 }
