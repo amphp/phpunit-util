92b3e23238ad4f33201d6759aad70ade3f07ea08
Niklas Keller <me@kelunik.com>
2019-02-11 21:12:41 +0100

    Make AsyncTestCase work correctly with PhpStorm

--- src/AsyncTestCase.php
+++ src/AsyncTestCase.php
@@ -5,42 +5,57 @@ namespace Amp\PHPUnit;
 use Amp\Loop;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 use function Amp\call;
-use function Amp\Promise\wait;
 
 /**
  * A PHPUnit TestCase intended to help facilitate writing async tests by running each test on the amphp Loop and
  * ensuring that the test runs until completion based on your test returning either a Promise or a Generator.
  */
-abstract class AsyncTestCase extends PHPUnitTestCase {
+abstract class AsyncTestCase extends PHPUnitTestCase
+{
 
     private $timeoutId;
+
     private $realTestName;
 
-    public function setName($name) {
-        $this->realTestName = $name;
+    public function setName($name)
+    {
         parent::setName('runAsyncTest');
+
+        $this->realTestName = $name;
     }
 
-    final public function runAsyncTest(...$args) {
+    public function getName($withDataSet = true)
+    {
+        return $this->realTestName;
+    }
+
+    final public function runAsyncTest(...$args)
+    {
         parent::setName($this->realTestName);
 
         $returnValue = null;
 
-        Loop::run(function() use(&$returnValue, $args) {
-            try {
-                $returnValue = yield call([$this, $this->realTestName], ...$args);
-            } finally {
-                if (isset($this->timeoutId)) {
-                    Loop::cancel($this->timeoutId);
+        try {
+            Loop::run(function () use (&$returnValue, $args) {
+                try {
+                    $returnValue = yield call([$this, $this->realTestName], ...$args);
+                } finally {
+                    if (isset($this->timeoutId)) {
+                        Loop::cancel($this->timeoutId);
+                    }
                 }
-            }
-        });
+            });
+        } finally {
+            Loop::set((new Loop\DriverFactory)->create());
+            \gc_collect_cycles(); // extensions using an event loop may otherwise leak the file descriptors to the loop
+        }
 
         return $returnValue;
     }
 
-    final protected function setTimeout(int $timeout) {
-        $this->timeoutId = Loop::delay($timeout, function() use($timeout) {
+    final protected function setTimeout(int $timeout)
+    {
+        $this->timeoutId = Loop::delay($timeout, function () use ($timeout) {
             Loop::stop();
             $this->fail('Expected test to complete before ' . $timeout . 'ms time limit');
         });
